480e17252ffe2378579e7d532bcca5e70a4b2b80
Tobias Fiechter <tobiasfiechter@gmail.com>
2015-11-27 09:55:21 +0100

    - new build tools - model class for a raspberry - first retrofit interface

---
+++ app/src/main/java/io/hotkey/rpi_monitorwatcher/model/Raspberry.java
@@ -0,0 +1,12 @@
+package io.hotkey.rpi_monitorwatcher.model;
+
+import java.net.InetSocketAddress;
+
+/**
+ * Created by Tobias Fiechter on 26.11.2015.
+ */
+public class Raspberry {
+
+    private InetSocketAddress address;
+
+}
--- app/src/main/java/io/hotkey/rpi_monitorwatcher/service/PiMonitorWatcherService.java
+++ app/src/main/java/io/hotkey/rpi_monitorwatcher/service/PiMonitorWatcherService.java
@@ -1,19 +1,49 @@
 package io.hotkey.rpi_monitorwatcher.service;
 
-import android.app.Service;
-import android.content.Intent;
-import android.os.IBinder;
-import android.support.annotation.Nullable;
+import retrofit.http.GET;
 
 /**
  * Created by Tobias Fiechter on 23.11.2015.
  */
-public class PiMonitorWatcherService extends Service {
+interface PiMonitorWatcherService {
 
 
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    @GET("/dynamic.json?_=1448530332355") //TODO: whats this timestamp?
+    StatusResponse getStatusResponse();
+
+    static class StatusResponse {
+        //Time
+        int[] localtime;
+        String uptime;
+
+        //Memory
+        float memory_available;
+        float memory_free;
+        float swap_used;
+
+        //CPU
+        float soc_temp;
+        String cpu_voltage;
+        int cpu_frequency;
+        String load1;
+        String load5;
+        String load15;
+
+        //SDCARD
+        float sdcard_boot_used;
+        float sdcard_root_used;
+        String scaling_governor;
+
+        //NETWORK
+        String net_send;
+        float net_received;
+
+        //UPDATES
+        String upgrade;
+    }
+
+    static class StatisticsResponse {
+
     }
 }
+
