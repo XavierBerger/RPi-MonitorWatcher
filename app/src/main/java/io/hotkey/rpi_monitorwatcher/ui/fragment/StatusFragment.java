8c9bdfe3c8c514c1315093297bb7d35db38c38b6
Tobias Fiechter <tobiasfiechter@gmail.com>
2016-02-11 11:49:54 +0100

    - updated gradle version - updated idea

--- app/src/main/java/io/hotkey/rpi_monitorwatcher/ui/fragment/StatusFragment.java
+++ app/src/main/java/io/hotkey/rpi_monitorwatcher/ui/fragment/StatusFragment.java
@@ -6,20 +6,41 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import io.hotkey.rpi_monitorwatcher.R;
+import io.hotkey.rpi_monitorwatcher.ui.adapters.StatusAdapter;
 
 /**
  * Created by Tobias Fiechter on 23.11.2015.
  */
 public class StatusFragment extends Fragment {
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.content_status, container, false);
+        mRecyclerView = (RecyclerView) root.findViewById(R.id.my_recycler_view);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this.getContext());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+
+        // specify an adapter (see also next example)
+        String[] data = {"Cheese", "Pepperoni", "Black Olives"};
+        mAdapter = new StatusAdapter(data);
+        mRecyclerView.setAdapter(mAdapter);
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.content_status, container, false);
